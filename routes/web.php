91f691280924b237ff3d23dad6527e0bcede34dc
ZulfanoRp <zulfano67@gmail.com>
2023-06-08 16:47:46 +0700

    add template for admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get("/", [HomeController::class, "index"]);
+
+Route::get("/users", [AdminController::class, "user"]);
+
 Route::get("/redirects", [HomeController::class, "redirects"]);
 // Route::get("/home", [HomeController::class, "index"]);
 
